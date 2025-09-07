4de65857b397109bef1137ac8f157fbfa758dcb6
CoolShivas <shivas2710cool00@gmail.com>
2025-09-07 17:32:52 +0530

    Formation of api folder and route.js file to make a get request using NextResponse.

---
+++ app/api/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  return NextResponse.json({
+    message: "Welcome to Next.js backend.",
+    success: true,
+  });
+}
+// // // Now, install the Thunder Client to see the result;
+// // // Open the Thunder Client and make a new request and enter the url (http://localhost:3000/api) and hit the send button.
+// // // You will get the response as (Status: 200 OK   Size: 41 Bytes    Time: 4.98 s) and below this
+// // // {
+// // //    "message": "Welcome to Next.js backend."
+// // //    "success": true
+// // // }
+// // //
+// // //
